c86ab829cb0f278d2b36b70571174aa54b3fdf1f
unknown <akarapon.sa.work@gmail.com>
2022-05-25 08:29:12 +0700

    first commit

---
+++ src/controllers/ocr.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from 'express'
+import { createWorker } from 'tesseract.js'
+
+const getText = (request: Request, response: Response, next: NextFunction) => {
+  const worker = createWorker({
+    logger: (m) => console.log(m),
+  });
+
+  (async () => {
+    await worker.load();
+    await worker.loadLanguage('eng');
+    await worker.initialize('eng');
+    const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
+    console.log(text);
+    await worker.terminate();
+  })();
+}
+
+export default getText
